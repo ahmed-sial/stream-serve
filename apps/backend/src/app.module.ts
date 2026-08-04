2da31bfb0871038062c75f35ae50bb3a1a144b79
Ahmed Hassan <0ahmed.sial@gmail.com>
2026-08-05 02:39:10 +0500

    feat(backend): setup lru cache module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './modules/database.module';
+import { LRUCacheModule } from './modules/cache.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    LRUCacheModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
